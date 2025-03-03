b4e084bcf85d6c2c3f5543037e15981b07d601e5
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2025-03-04 00:32:10 +0530

    Update rename_file.sh

--- file and test processing/rename_file.sh
+++ file and test processing/rename_file.sh
@@ -10,6 +10,11 @@ for file in "$FILE_PATH"/*.png; do
         filename=$(basename "$file" .png)
         # Create the new name using the current day
         new_name="${current_day}_${filename}.png"
+
+        # If you want to rename all file in .txt then
+        # file_name=$(basename "$file")
+        # new_name="${file_path}/${file_name%.*}.txt"
+        
         # Rename the file
         mv "$file" "${FILE_PATH}/${new_name}"
         echo "Renamed $file to ${FILE_PATH}/$new_name"
