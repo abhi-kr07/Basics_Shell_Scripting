f69430397348dfe55fb6c2c14df0cefe5de68e41
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2025-03-03 23:13:28 +0530

    Update empty_file_check.sh

--- file and test processing/empty_file_check.sh
+++ file and test processing/empty_file_check.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 file_path="/home/ubuntu/file_test_processing/folder.txt"
-if [[ -s $file_path ]];
+if [[ -f $file_path ]];
 then
-        echo "file is not empty"
+        if [[ -s $file_path ]];
+        then
+                echo "File is not empty"
+        else
+                echo "File is empty"
+        fi
 else
-        echo "file is empty"
+        echo "file is not there"
 fi
+
+# be done same as Directory
+folder_path="/home/ubuntu/folder"
+if [[ -d $folder_path ]];
+then
+        echo "Directory exists"
+        if [[ -z $(ls -A $folder_path) ]];
+        then
+                echo "But the Directory is empty"
+        else
+                echo "Directory is not empty"
+        fi
+fi
+
+
+
