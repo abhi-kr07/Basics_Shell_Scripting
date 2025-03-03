1aa731ea0671f41b9833ed391fd89507afe6de4f
abhi-kr07 <112303518+abhi-kr07@users.noreply.github.com>
2025-03-04 00:02:11 +0530

    Update tar_encrypt.sh

--- file and test processing/tar_encrypt.sh
+++ file and test processing/tar_encrypt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #
-# This will actually tar your file and then it will be encrypted
-#
+# This will actually tar your file, and then it will be encrypted
+# source_path will be like /home/ubuntu/folder
+# destination_path will be like /tmp/backup/"$timestamp"_backup_folder.tar.gz
 
+# can also add in crontab using crontab -e
+# ***** /path/of/your/backup.sh and make sure backup.sh has an executable permission
+
+timestamp=$(date +"%y-%m-%d_%H-%M-%S")
 source_path=$1
 destination_path=$2
 
